043ea078bf5ad98be9c5ea278bbd2123460af676
DESKTOP-85CI733\jacob <jacobanthonymurphy@gmail.com>
2023-01-08 12:04:52 -0500

    Added Google Analytics. Woohoo!

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ config.autoAddCss = false
 import '../styles/scss/global.scss'
 import {useEffect} from 'react'
 
+import Head from 'next/head'
+import Script from 'next/script'
+
 function MyApp({ Component, pageProps }) {
 
   /* import bootstrap js */
@@ -15,7 +18,19 @@ function MyApp({ Component, pageProps }) {
   /* per-page layout */
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-FEBED8HBLD" />
+      <Script id="google-analytics" strategy="afterInteractive" >
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-FEBED8HBLD');
+        `}
+      </Script>
+    {getLayout(<Component {...pageProps} />)}
+  </>;
 }
 
 export default MyApp
