3b2e8583f82bf9a3466c780b3a9f656ac673303e
DESKTOP-85CI733\jacob <jacobanthonymurphy@gmail.com>
2024-01-20 13:06:36 -0500

    3

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,12 +1,13 @@
 import { getDocuments } from 'outstatic/server'
 
 export default async function Index() {
-    const posts = await getData()
-    return posts.map((post) => <h1 key={post.slug}>{post.title}</h1>)
+    /*const posts = await getData()
+    return posts.map((post) => <h1 key={post.slug}>{post.title}</h1>)*/
+    return <h1>test</h1>
 }
 
 async function getData() {
-    const posts = getDocuments('posts', ['title', 'slug'])
+    /*const posts = getDocuments('posts', ['title', 'slug'])*/
 
-    return posts
+    return {}
 }
